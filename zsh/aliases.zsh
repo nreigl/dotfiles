09fe610bcd26c233c34977c31ab4a110fe432c7c
Nicolas Reigl <nicolas.reigl@gmail.com>
2016-10-14 23:07:31 +0300

    major updates to zshr

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
-# Use colors in coreutils utilities output
+## Alias ##
+## Use colors in coreutils utilities output
 alias ls='ls --color=auto'
 alias grep='grep --color'
 
@@ -6,6 +7,21 @@ alias grep='grep --color'
 alias ll='ls -lah'
 alias la='ls -A'
 alias l='ls'
+alias lh="ls -a | egrep "^\."" # alias dotfiles
+# List only directories
+ alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
+
+# show only hidden files (dot files)
+# alias hidden='ls -a | grep "^\."'
+# show only hidden files with additional arguments
+# hidden() { ls -a "$@" | grep '^\.'; }
+
+# Get macOS Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; sudo gem cleanup'
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
 
 # Aliases to protect against overwriting
 alias cp='cp -i'
@@ -13,7 +29,37 @@ alias mv='mv -i'
 
 # git related aliases
 alias gag='git exec ag'
+# Remove git from a project
+alias ungit="find . -name '.git' -exec rm -rf {} \;"
 
+
+# alias Dropbox
+alias db="cd ~/Dropbox/"
+# quick source
+alias zs="source ~/.zshrc"
+
+alias zshconfig="mate ~/.zshrc"
+alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# cd to git root directory
+alias cdgr='cd "$(git root)"'
+
+# open html in chrome
+alias chrome="open -a \"Google Chrome\""
+
+#copy output of last command to clipboard
+alias cl="fc -e -|pbcopy"
+
+# copy the working directory path
+alias cpwd='pwd|tr -d "\n"|pbcopy'
+
+# Get your current public IP
+alias ip="curl icanhazip.com"
+
+
+
+
+## Functions ##
 # Update dotfiles
 function dfu() {
     (
@@ -30,8 +76,6 @@ function syspip3() {
     PIP_REQUIRE_VIRTUALENV="" pip3 "$@@"
 }
 
-# cd to git root directory
-alias cdgr='cd "$(git root)"'
 
 # Create a directory and cd into it
 function mcd() {
@@ -42,17 +86,6 @@ function mcd() {
 function jump() {
     cd "$(dirname ${1})"
 }
-
-# cd replacement for screen to track cwd (like tmux)
-function scr_cd()
-{
-    builtin cd $1
-    screen -X chdir $PWD
-}
-
-if [[ -n $STY ]]; then
-    alias cd=scr_cd
-fi
 
 # Go up [n] directories
 function up()
@@ -90,19 +123,38 @@ function nonascii() {
 }
 
 # Fetch pull request
-
 function fpr() {
     git fetch "git@github.com:${2}/${1}" "${3}:${2}/${3}"
 }
 
-# Serve current directory
+# Change iterm2 profile. Usage it2prof ProfileName (case sensitive)
+it2prof() { echo -e "\033]50;SetProfile=$1\a" }
 
-function serve() {
-    ruby -run -e httpd . -p "${1:-8080}"
+# Open in PathFinder from the Terminal
+pf () { open -a "Path Finder.app" $1; }
+
+ester() {
+    search=""
+    echo "Googling: $@"
+    for term in $@; do
+        search="$search%20$term"
+    done
+    opens "https://www.ester.ee/search?q=$search"
 }
 
-# Mirror a website
-alias mirrorsite='wget -m -k -K -E -e robots=off'
 
-# Mirror stdout to stderr, useful for seeing data going through a pipe
-alias peek='tee >(cat 1>&2)'
+google() {
+    search=""
+    echo "Googling: $@"
+    for term in $@; do
+        search="$search%20$term"
+    done
+    xdg-open "http://www.google.com/search?q=$search"
+}
+
+
+## Export path directions ##
+# path to anaconda3
+export PATH="/Users/nicolasreigl/anaconda3/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/TeX/texbin"
+# open matlab from the terminal
+export PATH=/Applications/MATLAB_R2016a.app/bin:$PATH
--- zshrc
+++ zshrc
@@ -49,60 +49,38 @@ ZSH_THEME="cobalt2" # agnoster or #fino-time
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(extract git brew osx sublime z matlab autoenv zsh-syntax-highlighting)
+plugins=(compleat extract git brew osx sublime z matlab autoenv zsh-syntax-highlighting)
 
 # User configuration
+# Settings
+source ~/.zsh/settings.zsh
 
-export PATH="/Users/nicolasreigl/anaconda3/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/TeX/texbin"
-# export MANPATH="/usr/local/man:$MANPATH"
+# Bootstrap
+source ~/.zsh/bootstrap.zsh
 
-# Matlab
-export PATH=/Applications/MATLAB_R2016a.app/bin:$PATH
+
+# Aliases
+source ~/.zsh/aliases.zsh
+
+
+# Syntax highlighting
+source ~/.zsh/syntax.zsh
 
 source $ZSH/oh-my-zsh.sh
+# source $ZSH/z.sh
 
 # You may need to manually set your language environment
 # export LANG=en_US.UTF-8
 
 
-
-
-
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# ssh
-# export SSH_KEY_PATH="~/.ssh/dsa_id"
-
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
 # plugins, and thes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
 
-# alias ll=ls-l"
-# alias db = "cd ~/Dropbox/"
-# alias zs = "source ~/.zshrc"
-
-# Matlab alias
-# alias matlab='/usr/local/MATLAB/R2016a/bin/matlab -desktop'
-
-# Change iterm2 profile. Usage it2prof ProfileName (case sensitive)
-it2prof() { echo -e "\033]50;SetProfile=$1\a" }
-
-# Open in PathFinder from the Terminal
-pf () { open -a "Path Finder.app" $1; }
+# include Z, yo: does not work for me via oh-my-zsh plugins
+source /Users/nicolasreigl/dotfiles/zsh/plugins/z/z.sh
 
 # Syntax highlighting
 # source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zshit
+source /Users/nicolasreigl/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
